3198f3833e40805a22a37073e3b089a366edd4c2
maheryfit <mahery_fitahiana@yahoo.com>
2023-03-07 12:08:31 +0300

    sprint1

---
+++ etu1821/framework/servlet/FrontServlet.java
@@ -0,0 +1,28 @@
+package etu1821.framework.servlet;
+
+import java.io.*;
+import java.util.HashMap;
+
+import etu1821.servlet.Mapping;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+
+public class FrontServlet extends HttpServlet {
+    public void init() {
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    public void destroy() {
+    }
+}
