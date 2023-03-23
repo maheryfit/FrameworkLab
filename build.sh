062793701ec2132bf1f25af63a771864bbd1a82a
maheryfit <mahery_fitahiana@yahoo.com>
2023-03-23 11:42:33 +0300

    changement

---
+++ build.sh
@@ -0,0 +1,1 @@
+jar cf jw.jar .
---
+++ test-framework/WEB-INF/src/mg/model/Emp.java
@@ -0,0 +1,12 @@
+package mg.model;
+
+import etu1821.annotation.Url;
+
+public class Emp {
+
+  @Url("emp-all")
+  public int findAll() {
+    return 0;
+  }
+    
+}
