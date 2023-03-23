c8ed81adb0c2fd00ffbfa3e432016f848d87e509
maheryfit <mahery_fitahiana@yahoo.com>
2023-03-23 21:45:43 +0300

    first commit

--- framework/src/etu1821/servlet/ModelView.java
+++ framework/src/etu1821/servlet/ModelView.java
@@ -4,9 +4,9 @@ public class ModelView {
     private String view;
 
     private void setView(String view) {
-        if (view.equals("")) {
+        if (view.equals(""))
             throw new IllegalArgumentException("The view must be a filename", new Throwable("View equals to nothing"));
-        }
+
         this.view = view;
     }
 
