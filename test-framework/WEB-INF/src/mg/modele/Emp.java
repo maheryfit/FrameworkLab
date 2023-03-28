5f49113b44e78fd8d39cc4c61a4ad9ca34a27582
maheryfit <mahery_fitahiana@yahoo.com>
2023-03-28 07:44:40 +0300

    sprint 5 mandeha

--- test-framework/WEB-INF/src/mg/modele/Emp.java
+++ test-framework/WEB-INF/src/mg/modele/Emp.java
@@ -1,10 +1,11 @@
 package mg.modele;
 
 import etu1821.annotation.Url;
+import etu1821.servlet.ModelView;
 
 public class Emp {
-    @Url("emp-get")
-    public int getAll() {
-        return 0;
+    @Url({ "/", "" })
+    public ModelView getAll() {
+        return new ModelView("test.jsp");
     }
 }
